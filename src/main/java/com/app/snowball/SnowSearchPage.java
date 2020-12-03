31145e764fdeedf8bd9d3eeb5f8a257743058b78
zhangcheng170245 <170902823@qq.com>
2020-12-03 15:44:12 +0800

    雪球app自动化po模式

---
+++ src/main/java/com/app/snowball/SnowSearchPage.java
@@ -0,0 +1,12 @@
+package com.app.snowball;
+
+/**
+ * @Author: zhangcheng
+ * @Description: 搜索功能
+ * @Date: 2020/12/2 21:10
+ * @Version: 1.0
+ */
+public class SnowSearchPage extends SnowBasePage {
+
+
+}

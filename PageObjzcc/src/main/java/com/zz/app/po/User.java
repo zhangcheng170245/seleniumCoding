52fb5467aac04176be16659f48cb1f293c0a594d
zhangcheng170245 <170902823@qq.com>
2021-03-25 23:36:51 +0800

    使用json参数化用的实体类

---
+++ PageObjzcc/src/main/java/com/zz/app/po/User.java
@@ -0,0 +1,12 @@
+package com.zz.app.po;
+
+/**
+ * @Author: zhangcheng
+ * @Description: 对应json的实体类
+ * @Date: 2021/3/25/025 23:15
+ * @Version: 1.0
+ */
+public class User {
+    public String userName;
+    public String userPhone;
+}

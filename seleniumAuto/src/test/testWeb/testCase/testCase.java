d8a356c21f6cb7ff40ca5baddd6bae3855bd8e38
zhangcheng <170902823@qq.com>
2021-09-20 10:28:35 +0800

    web自动自动化实现元素高亮1

--- seleniumAuto/src/test/testWeb/testCase/testCase.java
+++ seleniumAuto/src/test/testWeb/testCase/testCase.java
@@ -8,6 +8,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import util.FakerUtils;
 
+
 /**
  * @ProjectName: coding
  * @Package: PACKAGE_NAME

b4a3b8acf0286e7b08e291d66d06010d86b1c1b6
zhangcheng <170902823@qq.com>
2021-09-20 10:23:36 +0800

    web自动自动化实现元素高亮

--- seleniumAuto/src/test/testWeb/testCase/testCase.java
+++ seleniumAuto/src/test/testWeb/testCase/testCase.java
@@ -3,9 +3,7 @@ package testCase;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
-import org.openqa.selenium.By;
-import org.openqa.selenium.TakesScreenshot;
-import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import util.FakerUtils;
@@ -22,19 +20,19 @@ import util.FakerUtils;
 public class testCase  {
     public static WebDriver driver;
     public static WebDriverWait webDriverWait;
+    public static WebElement element;
 
     @BeforeAll
     static void initData(){
         //加载驱动
         driver=new ChromeDriver();
-
         // 设置全局隐式等待 5S
         // driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
         webDriverWait= new WebDriverWait(driver,5L);
     }
 
     @Test
-    void Login(){
+    void Login() throws InterruptedException {
         //读取测试链接
         driver.get("https://home.testing-studio.com/");
         //获取登录按钮
@@ -45,7 +43,13 @@ public class testCase  {
         driver.findElement(By.id("login-account-name")).sendKeys("*");
 
         driver.findElement(By.id("login-account-password")).clear();
+        //获取登录的高亮元素
+        element = driver.findElement(By.id("login-account-password"));
+        //高亮
+        highLight(element);
+        Thread.sleep ( 2000 );
         driver.findElement(By.id("login-account-password")).sendKeys("*");
+        JavascriptExecutor js= (JavascriptExecutor) driver;
         driver.findElement(By.id("login-button")).click();
         // 截图
         FakerUtils.snapshot((TakesScreenshot)driver,"截图"+FakerUtils.getTimeStamp()+".jpg");
@@ -73,5 +77,20 @@ public class testCase  {
     static void tearDown(){
         driver.quit();
     }
+
+    // 封装展示高亮的方法
+    public  static void highLight(WebElement element){
+        //创建一个JavascriptExecutor对象
+        JavascriptExecutor js= (JavascriptExecutor) driver;
+        // 设置组件高亮
+        //js.executeAsyncScript("arguments[0].style.border=\"5px solid red\"", webElement);
+        js.executeAsyncScript("element = arguments[0];" +
+                "original_style = element.getAttribute('style');" +
+                "element.setAttribute('style', original_style + \";" +
+                "background: yellow; border: 2px solid red;\");" +
+                "setTimeout(function(){element.setAttribute('style', original_style);}, 1000);", element);
+
+    }
+
 
 }
